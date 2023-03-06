5b8bc36d4f6798059f4ca02729c779ce39f213dd
ulxsth <ulxsth@gmail.com>
2023-03-06 06:48:59 +0000

    [UPDATE] ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/top'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/' => 'books#top'
+  resources :books
 end
