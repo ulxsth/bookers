7423240cf114d73a64a344ad0907fa25723c3a7a
ulxsth <ulxsth@gmail.com>
2023-03-07 02:32:52 +0000

    [ADD] create,edit時にフラッシュメッセージを表示するように

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
+    flash[:success] = "Book was successfully created."
     redirect_to book_path(book.id)
   end
 
@@ -29,6 +30,7 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     
     if book.update(book_params)
+      flash[:success] = "Book was successfully updated."
       redirect_to book_path(book.id)
     else
       render :edit
